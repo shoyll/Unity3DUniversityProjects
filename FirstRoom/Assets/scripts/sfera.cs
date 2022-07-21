e00a4e40c26e52f73d6c24af8002fad3d22b45b0
shoyll <michaeldesantis1998@gmail.com>
2022-07-21 15:59:36 +0200

    aggiunta sfera a FirstRoom

---
+++ FirstRoom/Assets/scripts/sfera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sfera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = new Vector3();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    //variabile di istanza
+    Vector3 initialPos;
+
+
+}
