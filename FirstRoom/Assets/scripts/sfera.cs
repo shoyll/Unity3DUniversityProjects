132a54d69932c13f061e0d1deed60b6ea819b421
shoyll <michaeldesantis1998@gmail.com>
2022-07-24 00:41:47 +0200

    new materials and Skybox

--- FirstRoom/Assets/scripts/sfera.cs
+++ FirstRoom/Assets/scripts/sfera.cs
@@ -7,17 +7,36 @@ public class sfera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        initialPos = new Vector3();   
+        pos = transform.position;
+
+    /*
+        posInit = GetComponent<Transform>(); // è una variabile Transorm
+        // pos = (Transform) GetComponent(typeof(Transform)); forma alternativa 
+    */  
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        pos = transform.position;
+        if (Input.anyKey)
+        Debug.Log(pos);
+        
+        if (Input.GetMouseButtonDown(0)) {
+            pos = new Vector3(0, 0, (pos.z + 1));
+        }
       
     }
 
+
     //variabile di istanza
-    Vector3 initialPos;
-
-
+    
+    public Vector3 pos;
+    
+    //public Transform posInit;
+    //public Transform pos; 
+    //public float moveSpeed;
+    //public float turnSpeed;
+    //public GameObject cube;
 }
